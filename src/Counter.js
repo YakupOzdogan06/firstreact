2e23a30bcf97ddf2fda06974aea97645efdc0447
Yakup <105318975+YakupOzdogan06@users.noreply.github.com>
2022-10-06 11:17:48 +0200

    first lesson for react completed

---
+++ src/Counter.js
@@ -0,0 +1,16 @@
+import React, { useState } from "react";
+
+const Counter = () => {
+  const [count, setCount] = useState(0);
+  return (
+    <div>
+      <h4>Counter:{count}</h4>
+      <button onClick={() => setCount(count + 1)}>+</button>
+      <button onClick={() => (count === 0 ? setCount(0) : setCount(count - 1))}>
+        -
+      </button>
+    </div>
+  );
+};
+
+export default Counter;
---
+++ src/Navbar.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Navbar = () => {
+  return <div>Code Diaries</div>;
+};
+
+export default Navbar;
